f952ab7feabd8424c7c697cfe18bb09f28827d8c
adi6499 <83864698+adi6499@users.noreply.github.com>
2023-08-19 16:23:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let contain = document.querySelector('.contain')
+       
+for(let i=0;i < 400;i++){
+    contain.innerHTML+=`<div class="star"></div>`
+    let star = document.querySelectorAll(".star")
+    star[i].style.left=`${Math.random() * window.innerWidth}px`
+    star[i].style.top=`${Math.random() * window.innerHeight}px`
+    let size = Math.random() * 5 + "px"
+    star[i].style.width=`${size}`
+    star[i].style.height=`${size}`
+    star[i].style.animationDelay=`${Math.random() * 10}s`
+}
+let songs = document.getElementById("songs")
+
+songs.play()
+
