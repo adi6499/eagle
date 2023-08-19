1b7ec5c75141c9ad8c7d5f54e680573159d31644
adi6499 <83864698+adi6499@users.noreply.github.com>
2023-08-19 15:01:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-let head = document.querySelector(".head")
-let title = document.querySelector(".title")
-let infoEagle = document.querySelector(".infoEagle")
-let card = document.querySelector(".card")
-let eagle = document.getElementById("eagle")
-let show = document.getElementById("show")
-let mountain = document.getElementById("mountain")
-let i = 0.5;
-
-
-
-
-window.addEventListener("scroll", () => {
-    let top = document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = Math.floor(top / height * 100);
-    eagle.style.transform = `translateY(-${i * scrolled}%)`
-    mountain.style.transform = `translateY(-${i * scrolled}%)`
-    head.style.transform = `translateY(${i * scrolled}%)`
-    if ((i * scrolled) >= 45) {
-        head.style.opacity = 0
-        title.classList.add("titleOpacity")
-
-    } else {
-        head.style.opacity = 10
-        title.classList.remove("titleOpacity")
-    }
-})
-
-
-//this is for show eagle info section and  
-//show is add event listener to target the eagle info to increase height
-
-
-show.addEventListener("click", (e) => {
-    if (e.target.textContent == "Show👇More") {
-        show.innerHTML = "Show👆Less"
-        infoEagle.classList.add("showInfoEagle")
-        card.classList.add("showInfoEagle")
-        show.style.background = "royalblue"
-    } else if (e.target.textContent == "Show👆Less") {
-        show.innerHTML = "Show👇More"
-        infoEagle.classList.remove("showInfoEagle")
-        card.classList.remove("showInfoEagle")
-        show.style.background = "black"
-    }
-
-})
+  let contain = document.querySelector('.contain')
+       
+        for(let i=0;i < 1000;i++){
+            contain.innerHTML+=`<div class="star"></div>`
+            let star = document.querySelectorAll(".star")
+            star[i].style.left=`${Math.random() * window.innerWidth}px`
+            star[i].style.top=`${Math.random() * window.innerHeight}px`
+            let size = Math.random() * 5 + "px"
+            star[i].style.width=`${size}`
+            star[i].style.height=`${size}`
+            star[i].style.animationDelay=`${Math.random() * 10}s`
+        }
+        
